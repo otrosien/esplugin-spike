bc11ba2f0f72edefd194ea3686c1050daca0097e
Oliver Trosien <otrosien@epages.com>
2016-04-26 20:38:34 +0200

    switch to maven

--- src/test/java/com/example/PhoneticPluginTest.java
+++ src/test/java/com/example/PhoneticPluginTest.java
@@ -3,25 +3,26 @@ package com.example;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.elasticsearch.common.inject.Injector;
 import org.elasticsearch.common.inject.ModulesBuilder;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.PluginsModule;
 import org.elasticsearch.plugins.PluginsService;
 import org.junit.Test;
-
-import com.example.AnalysisPhoneticPlugin;
-import com.example.PhoneticAnalysisBinderProcessor;
 
 public class PhoneticPluginTest {
 
     @Test
     public void pluginLoads() {
         Settings settings = Settings.builder().build();
-        PluginsService pluginsService = new PluginsService(settings, null, null,
-                Arrays.asList(AnalysisPhoneticPlugin.class));
+        List<Class<? extends Plugin>> plugins = new ArrayList<>();
+        plugins.add(AnalysisPhoneticPlugin.class);
+        PluginsService pluginsService = new PluginsService(
+                settings, null, null, plugins);
 
         ModulesBuilder modules = new ModulesBuilder();
         modules.add(new PluginsModule(pluginsService));
